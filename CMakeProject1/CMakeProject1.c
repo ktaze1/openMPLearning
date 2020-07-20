f978935f5a246c9a16284557ce323271e39bfedf
Kaan Taze <bkaantaze@gmail.com>
2020-07-21 00:34:04 +0300

    init

---
+++ CMakeProject1/CMakeProject1.c
@@ -0,0 +1,19 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+int main()
+{
+	int i;
+	int threadID = 0;
+#pragma omp parallel for private(i, threadID)
+	for (i = 0; i < 16; i++) {
+		threadID = omp_get_thread_num();
+		#pragma omp critical 
+		{
+			printf("Thread %d reporting\n", threadID); 
+		}
+	}
+	return 0;
+}
---
+++ CMakeProject1/CMakeProject1.h
@@ -0,0 +1,9 @@
+﻿// CMakeProject1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <stdio.h>
+#include <omp.h>
+
+// TODO: Reference additional headers your program requires here.
